89eaf0b61ef4a2c70b177be0f0b38c9d845c3706
Eduardo da Silva <eduardo.2599@hotmail.com>
2018-10-21 19:13:58 -0200

    Definido os fillable e os relacionamentos das models

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'stock',
+        'price',
+        'category_id',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
 }
